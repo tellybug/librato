600ee426a9df131ba6a695920fa9ab5762296a3d
Matt Millar <matt@emillar.com>
2012-02-27 17:53:38 +0000

    Moved keys into separate, local only file

--- test/test_librato.py
+++ test/test_librato.py
@@ -1,3 +1,37 @@
+# Copyright (c) 2012 Matt Millar http://tellybug.com
+#
+# Permission is hereby granted, free of charge, to any person obtaining a
+# copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish, dis-
+# tribute, sublicense, and/or sell copies of the Software, and to permit
+# persons to whom the Software is furnished to do so, subject to the fol-
+# lowing conditions:
+#
+# The above copyright notice and this permission notice shall be included
+# in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+# OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABIL-
+# ITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT
+# SHALL THE AUTHOR BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, 
+# WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+# IN THE SOFTWARE.
+
+"""
+
+to run the testcode create a file
+
+acc_keys.py
+
+with two lines
+
+LibratoUsername = "email@example.com"
+LibratoAPIKey = "APIUSERKEYHERE"
+
+"""
+
 import unittest
 import sys
 
@@ -10,12 +44,14 @@ sys.path.insert(0, os.path.abspath(os.path.join(SOURCE_ROOT, os.path.pardir)))
 from librato.connection import LibratoConnection
 from librato import exceptions
 
+from acc_keys import LibratoUsername, LibratoAPIKey
+
 class TestLibratoAPI(unittest.TestCase):
 
     def setUp(self):
         # these are test account keys - replace with your own!
-        self.libratoUser = "matt+test@tellybug.com"
-        self.libratoAPIKey = "397e1696c87e35cb9b99bf557026ccdca3b1fe17f3ac94f669c8a051d7f7b8b7"
+        self.libratoUser = LibratoUsername
+        self.libratoAPIKey = LibratoAPIKey
 
     def test_create_gauge(self):
         connection = LibratoConnection(self.libratoUser,self.libratoAPIKey)
