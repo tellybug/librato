8fe8ee7d2c4253aac5fa92537cd14eeda16403a2
Matt Millar <matt@emillar.com>
2012-02-29 13:57:26 +0000

    Bumped version number to 0.3-pre1

--- librato/__init__.py
+++ librato/__init__.py
@@ -19,6 +19,6 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
 # IN THE SOFTWARE.
 
-__version__ = "0.2"
+__version__ = "0.3-pre1"
 
 from connection import LibratoConnection, connect
